938cbdb956af6aa026cb9dd183311c2e377f6b79
Quan <quannguyen157716@gmail.com>
2019-04-20 18:32:30 +0700

    Initial commit

---
+++ Assets/Scripts/GameDriver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDriver : MonoBehaviour {
+
+	public GameObject block;
+	Vector3 pos1;
+	// Use this for initialization
+	void Start () {
+		pos1=new Vector3(-0.01f,-2.5f,3);
+		Instantiate(block,pos1,Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
---
+++ Assets/Scripts/TriangleGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  TriangleGenerator: MonoBehaviour {
+
+	Mesh mesh;
+	Vector3[] verticles;
+	int[] triangles;
+	// Use this for initialization
+	void Start () {
+		mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		CreateShape();
+		UpdateMesh();
+	}
+	
+	void CreateShape()
+	{
+		verticles =new Vector3[]
+		{
+			new Vector3 (0,0,0),//0
+			new Vector3 (1,0,2),//1
+			new Vector3(2,0,0),//2
+			new Vector3(2,0,3)//3
+		};
+
+		triangles =new int[]
+		{
+			0, 1, 2, 2, 3, 1
+		};
+	}
+
+	void UpdateMesh()
+	{
+		mesh.Clear();
+		mesh.vertices=verticles;
+		mesh.triangles=triangles;
+	}
+}
