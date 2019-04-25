e189bac2e38189bdca7600f3b01d7619f2a445a8
Quan <quannguyen157716@gmail.com>
2019-04-25 22:59:39 +0700

    Neww

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour {
+
+	float distance = 3;//distance from camera to object
+
+    void OnMouseDrag()
+
+	{
+
+        Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+        transform.position = objPosition;
+
+	}
+
+}
